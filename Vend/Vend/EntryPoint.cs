bfdb4c8381eeb992ea73fa8c133b7d59cbe86a3d
BrianWilbur <brian.m.wilbur@gmail.com>
2016-04-06 20:02:23 -0400

    Initial commit.

---
+++ Vend/Vend/EntryPoint.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vend
+{
+	class EntryPoint
+	{
+		/**
+		 * Basic entry point for application.
+		 */
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello world!");
+			Console.Read();
+		}
+	}
+}
